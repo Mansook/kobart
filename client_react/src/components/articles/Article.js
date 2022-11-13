67d2cfca8e0a32ce7aa3f016813d921f2f277e0c
junseokim <hazzisss5249@gmail.com>
2022-11-13 23:32:12 +0900

    design_11_13

--- client_react/src/components/articles/Article.js
+++ client_react/src/components/articles/Article.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./css/article.css"
 
 const Article = () => {
   const { state } = useLocation();
@@ -14,12 +15,14 @@ const Article = () => {
   } = state;
 
   return (
-    <div>
-      <div>제목:{article_name}</div>
-      <div>내용:{article_main}</div>
-      <div>요약:{article_summary}</div>
-      <div>키워드:{keywords}</div>
-      <div>리포터:{reporter}</div>
+    <div className="article_main">
+      <hr/>
+      <div className="article_box">제목:{article_name}</div>
+      <div className="article_box">내용:{article_main}</div>
+      <div className="article_box">요약:{article_summary}</div>
+      <div className="article_box">키워드:{keywords}</div>
+      <div className="article_box_reporter">리포터:{reporter}</div>
+      <hr/>
     </div>
   );
 };
--- client_react/src/components/auth/AuthForm.js
+++ client_react/src/components/auth/AuthForm.js
@@ -1,39 +1,53 @@
 import { Link } from "react-router-dom";
+import "./css/auth.css"
 const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
   const text = type;
 
   return (
+    // <div className="auth_main">
     <div>
       <h3>{text}</h3>
-      <form onSubmit={onSubmit}>
-        <input
-          autoComplete="username"
-          name="email"
-          placeholder="이메일"
-          onChange={onChange}
-          value={form.email}
-        />
-        <input
-          autoComplete="new-password"
-          name="password"
-          placeholder="비밀번호"
-          type="password"
-          onChange={onChange}
-          value={form.password}
-        />
-        {type === "register" && (
+      <form classname = "form_style" onSubmit={onSubmit}>
+        <div>
+          <div classname = "input_name"> 아이디 </div>
+          <input
+            autoComplete="username"
+            name="email"
+            placeholder="이메일"
+            onChange={onChange}
+            value={form.email}
+          />
+        </div>
+        <br />
+        <div>
+          <div classname = "input_name"> 비밀번호</div>
           <input
             autoComplete="new-password"
-            name="passwordConfirm"
-            placeholder="비밀번호 확인"
+            name="password"
+            placeholder="비밀번호"
             type="password"
             onChange={onChange}
-            value={form.passwordConfirm}
+            value={form.password}
           />
+        </div>
+        <br />
+        {type === "register" && (
+          <div>
+            <div classname = "input_name"> 비밀번호 확인</div>
+            <input
+              autoComplete="new-password"
+              name="passwordConfirm"
+              placeholder="비밀번호 확인"
+              type="password"
+              onChange={onChange}
+              value={form.passwordConfirm}
+            />
+          </div>
         )}
+        <br />
         <button type="submit">제출</button>
       </form>
-      <div>
+      <div classname = "login_register_link">
         {type === "login" ? (
           <Link to="/register">회원가입</Link>
         ) : (
